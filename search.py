e61de5972f09a72a324be961f871c2536e9eec86
vishal-pandey <pandeyvishal25@yahoo.com>
2018-02-16 01:33:08 +0530

    with try

--- search.py
+++ search.py
@@ -10,22 +10,26 @@ vlinks = []
 def search(theLink):
 	vlinks.append(theLink)
 	print(theLink+" added .. \n")
-	res = requests.get(theLink)
-	soup = bs4.BeautifulSoup(res.text, 'lxml')
-	hi = soup.select('a')
-	
-	for x in hi:
-		if 'href' in x.attrs:	
-			link = x['href']
-			linko = urlparse(link)
-			if link[0:4] == 'http':
-				slink = linko.scheme+'://'+linko.netloc
+	try :
+		res = requests.get(theLink)
+		soup = bs4.BeautifulSoup(res.text, 'lxml')
+		hi = soup.select('a')
+		
+		for x in hi:
+			if 'href' in x.attrs:	
+				link = x['href']
+				linko = urlparse(link)
+				if link[0:4] == 'http':
+					slink = linko.scheme+'://'+linko.netloc
 
-				if slink not in links:
-					links.append(slink)
-					fo = open("output.html", "a")
-					fo.write('<a href="'+slink+'">'+slink+'</a><br>')
-					fo.close()
+					if slink not in links:
+						links.append(slink)
+						fo = open("output.html", "a")
+						fo.write('<a href="'+slink+'">'+slink+'</a><br>')
+						fo.close()
+	except error :
+		print("error occured")
+
 
 
 
