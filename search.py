1b4ebcfe099013b1cfac3725a83357413ac9a78e
vishal-pandey <pandeyvishal25@yahoo.com>
2018-02-16 01:52:47 +0530

    latest code

--- search.py
+++ search.py
@@ -4,35 +4,25 @@ from urllib.parse import urlparse
 
 links = ['https://www.vishalpandey.xyz']
 
-vlinks = []
-
-
 def search(theLink):
-	vlinks.append(theLink)
-	print(theLink+" added .. \n")
-	try :
-		res = requests.get(theLink)
-		soup = bs4.BeautifulSoup(res.text, 'lxml')
-		hi = soup.select('a')
-		
-		for x in hi:
-			if 'href' in x.attrs:	
-				link = x['href']
-				linko = urlparse(link)
-				if link[0:4] == 'http':
-					slink = linko.scheme+'://'+linko.netloc
+	# print(theLink+" added .. \n")
+	res = requests.get(theLink)
+	soup = bs4.BeautifulSoup(res.text, 'lxml')
+	hi = soup.select('a')
+	
+	for x in hi:
+		if 'href' in x.attrs:	
+			link = x['href']
+			linko = urlparse(link)
+			if link[0:4] == 'http':
+				slink = linko.scheme+'://'+linko.netloc
 
-					if slink not in links:
-						links.append(slink)
-						fo = open("output.html", "a")
-						fo.write('<a href="'+slink+'">'+slink+'</a><br>')
-						fo.close()
-	except error :
-		print("error occured")
-
-
-
+				if slink not in links:
+					links.append(slink)
+					fo = open("output.html", "a")
+					fo.write('<a href="'+slink+'">'+slink+'</a><br>')
+					fo.close()
 
 for link in links:
-	print("searching .. "+link)
+	# print("searching .. "+link)
 	search(link)
